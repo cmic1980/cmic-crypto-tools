aea20c71c0570c9fa8892b6dd377831a0d0e2b7d
Yu.miao <yu.miao@hinacom.com>
2021-02-19 00:22:45 +0800

    symbol and contract list

--- src/renderer/strategy/stalking.js
+++ src/renderer/strategy/stalking.js
@@ -48,9 +48,52 @@ function combineKline(klineList) {
     return newList;
 }
 
+function calculateType(newList) {
+    // 2.1.2 顶底分型判断
+    let weekType = 0; // 0 未有分型， 1 顶分型， 2 底分型
+    let weekId = 0;
+    for (let i = 2; i < newList.length - 1; i++) {
+        let left = newList[i - 1];
+        let p = newList[i];
+        let right = newList[i + 1];
+
+        // 顶分型判断
+        if (weekType != 1) {
+            // 如果有某一根 K 线的高点都高于左右两侧 K 线的高点
+            if (p.high > left.high && p.high > right.high) {
+                // 并且这根 K 线的低点也高于左右两侧的 K 线的低点
+                if (p.low > left.low && p.low > right.low) {
+                    weekType = 1;
+                    weekId = p.id;
+                }
+            }
+        }
+
+        // 底分型判断
+        if (weekType != 2) {
+            // 如果有某一根 K 线的高点都低于左右两侧 K 线的高点
+            if (p.high < left.high && p.high < right.high) {
+                // 并且这根 K 线的低点也低于左右两侧的 K 线的低点
+                if (p.low < left.low && p.low < right.low) {
+                    weekType = 2;
+                    weekId = p.id;
+
+
+                }
+            }
+        }
+    }
+
+    let date = new Date();
+    date.setTime(weekId * 1000)
+    let xdate = new XDate(date)
+
+    let result = { "weekDate": xdate.toString("yyyy-MM-dd"), "weekType": weekType }
+    return result;
+}
+
 export default {
     calculate(symbol, cb) {
-
         market.getKline("1week", 2000, symbol, cb)
             .then(function (response) {
                 let klineList = response.data.data;
@@ -60,49 +103,9 @@ export default {
                 });
                 // 2.1.1 K 线合并
                 let newList = combineKline(klineList);
-                let weekType = 0; // 0 未有分型， 1 顶分型， 2 底分型
-                let weekId = 0;
-                // 2.1.2 顶底分型判断
-                for (let i = 2; i < newList.length - 1; i++) {
-                    let left = newList[i - 1];
-                    let p = newList[i];
-                    let right = newList[i + 1];
-
-                    // 顶分型判断
-                    if (weekType != 1) {
-                        // 如果有某一根 K 线的高点都高于左右两侧 K 线的高点
-                        if (p.high > left.high && p.high > right.high) {
-                            // 并且这根 K 线的低点也高于左右两侧的 K 线的低点
-                            if (p.low > left.low && p.low > right.low) {
-                                weekType = 1;
-                                weekId = p.id;
-                            }
-                        }
-                    }
-
-                    // 底分型判断
-                    if (weekType != 2) {
-                        // 如果有某一根 K 线的高点都低于左右两侧 K 线的高点
-                        if (p.high < left.high && p.high < right.high) {
-                            // 并且这根 K 线的低点也低于左右两侧的 K 线的低点
-                            if (p.low < left.low && p.low < right.low) {
-                                weekType = 2;
-                                weekId = p.id;
-
-
-                            }
-                        }
-                    }
-                }
-
-                let date = new Date();
-                date.setTime(weekId * 1000)
-
-                let xdate = new XDate(date)
-
-
-                let result = { "weekDate": xdate.toString("yyyy-MM-dd"), "weekType": weekType }
+                let result = calculateType(newList);
                 cb(result)
+
             })
             .catch(function (error) {
                 console.log(error);
